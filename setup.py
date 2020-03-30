d9b92eeebad9bfa7391cbe2db21c3ea6b2adf41d
jayarehart <joseph.arehart@colorado.edu>
2020-03-30 17:28:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@
 import os
 from setuptools import setup
 
-packages = []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
+# packages = []
+# root_dir = os.path.dirname(__file__)
+# if root_dir:
+#     os.chdir(root_dir)
 
-for dirpath, dirnames, filenames in os.walk('odym'):
-    # Ignore dirnames that start with '.'
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
+# for dirpath, dirnames, filenames in os.walk('odym'):
+#     # Ignore dirnames that start with '.'
+#     if '__init__.py' in filenames:
+#         pkg = dirpath.replace(os.path.sep, '.')
+#         if os.path.altsep:
+#             pkg = pkg.replace(os.path.altsep, '.')
+#         packages.append(pkg)
 
 setup(
     name='odym',
