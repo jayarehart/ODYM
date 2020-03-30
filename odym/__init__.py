bf653a9f3c4b8057d3a4704f98d55ef6dec13a3d
jayarehart <joseph.arehart@colorado.edu>
2020-03-30 17:55:40 +0100

    Update __init__.py

--- odym/__init__.py
+++ odym/__init__.py
@@ -1,3 +1,3 @@
-from .modules import ODYM_Classes as msc
-from .modules import ODYM_Functions as msf
-from .modules import dynamic_stock_model as dsm
+from .modules import ODYM_Classes.py as msc
+from .modules import ODYM_Functions.py as msf
+from .modules import dynamic_stock_model.py as dsm
