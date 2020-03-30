187b91b6e1b91db12b52c7181285b9119a9c8efb
jayarehart <joseph.arehart@colorado.edu>
2020-03-30 18:03:36 +0100

    Update __init__.py

--- odym/__init__.py
+++ odym/__init__.py
@@ -1,3 +1,3 @@
-from .ODYM_Classes import ODYM_Classes
-from .ODYM_Functions import ODYM_Functions
+# from .ODYM_Classes import ODYM_Classes
+# from .ODYM_Functions import ODYM_Functions
 from .dynamic_stock_model import DynamicStockModel
